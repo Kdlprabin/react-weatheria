289d7bdeab1e637f33606e836671f9721d31eed7
prabin <kdlprabin444@gmail.com>
2022-12-23 22:27:22 +0545

    miliseconds converted to hours and minutes fromat

--- src/context/WeatherState.js
+++ src/context/WeatherState.js
@@ -8,25 +8,31 @@ export const WeatherState = (props) => {
   function handleDate(time) {
     var date = "1475235770601";
     var d = new Date(parseInt(date, 10));
-    var ds = d.toString("MM/dd/yy HH:mm:ss");
-    console.log(ds);
+    var ds = d.toTimeString();
+    const hour = parseInt(ds.substring(0, 2), 10);
+    const min = parseInt(ds.substring(3, 5), 10);
+    return (`${hour} : ${min}`);
   }
   async function refresh() {
-    let response = await axios.get(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&appid=43aa310cf040eb7a9bc888de93a2c9dc&units=metric"
-    );
-    var data = {
-      temperature: response.data.main.feels_like,
-      description: response.data.weather[0].main,
-      temp_min: response.data.main.temp_min,
-      temp_max: response.data.main.temp_max,
-      wind_speed: response.data.wind.speed,
-      sunrise: response.data.sys.sunrise,
-      sunset: response.data.sys.sunset,
-    };
-    setWInfo(data);
+    try {
+      let response = await axios.get(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&appid=183d96102ce7ed547f6821e7d53efb51&units=metric"
+      );
+      var data = {
+        temperature: response.data.main.feels_like,
+        description: response.data.weather[0].main,
+        temp_min: response.data.main.temp_min,
+        temp_max: response.data.main.temp_max,
+        wind_speed: response.data.wind.speed,
+        sunrise: handleDate(response.data.sys.sunrise),
+        sunset: handleDate(response.data.sys.sunset),
+      };
+      setWInfo(data);
+    } catch (err) {
+      console.log(err);
+    }
   }
   return (
     <WeatherContext.Provider value={{ state: wInfo, update: refresh }}>
